aa1161d25a0b257823c3d090da9ae4a4a5edbcc6
parzival867 <nathyadiah@gmail.com>
2021-10-14 08:11:31 -0400

    add django-heroku

--- mydb/settings.py
+++ mydb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku 
 from pathlib import Path
 import os
 
@@ -131,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
